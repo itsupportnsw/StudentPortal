bdce2e210ec191227d1b4303eaae6fd98089f86d
aoddy10 <vobi69p@gmail.com>
2021-01-21 09:58:38 +1100

    fix: fetch student permission from server

---
+++ WebApp/js/login.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#loginBtn").click(async function () {
+    let data = {
+      studentID: $("#studentID").val(),
+      email: $("#email").val(),
+    };
+
+    // fetch student permission from api server
+    const response = await fetch(
+      "http://localhost:3000/api/v1/users/getUserPermission",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    let student = await response.json();
+
+    if (student.data.length <= 0) {
+      alert("Please check you student ID or Email address again.");
+      return;
+    }
+
+    // coding to open student detail page
+  });
+});
