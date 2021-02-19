be4f3d4c98e3bfd77b8ac55f8dc54e943b95a77a
Nutnaree-u <Nutnaree.u@gmail.com>
2021-02-19 16:07:17 +1100

    add payment page

--- WebApp/js/home.js
+++ WebApp/js/home.js
@@ -1,13 +1,23 @@
+ // Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+} 
+
 $(document).ready(function () {
+  
   //   get student detail from localstorage\
   let studentDetail = JSON.parse(localStorage.getItem("studentDetail"));
 
   let studentProfile = studentDetail.studentDetail;
   let timetable = studentDetail.enrolmentClass;
+ 
 
-  console.log(studentProfile);
-  console.log(timetable);
-
+console.log(studentDetail);
   // display student detail
   $("#studentID").html(studentProfile.studentID);
   $("#fullName").html(studentProfile.fullName);
@@ -21,32 +31,30 @@ $(document).ready(function () {
     var image = Array.prototype.map.call(data, function (ch) {
         return String.fromCharCode(ch);
     }).join('');
-    console.log("hi");
+
     return btoa(image);
 }
-console.log(studentProfile.photoData.data)
+ // console.log(studentProfile.photoData.data)
   var base64 = bufferToBase64(studentProfile.photoData.data); 
   var studentImage = "data:image/jpg;base64,"+base64;
 
-  console.log(studentImage);
+
 
   // change image source to ProfileImage. (search jquery)
 
   $("#ProfileImg").attr('src',studentImage);
 
-console.log("hello");
+
 
 
   // display timetable
   let tableHtml = "";
 
   timetable.forEach((item, index) => {
-    tableHtml += '<div class="Card col12 col-md-4"><div><span class="th">Class Name : </span><span>'+
+    tableHtml += '<div class="Card col-12 col-md-4 "><div><span class="th">Class Name : </span><span>'+
     item.className +'</span></div><div><span class="th">Unit Name :  </span><span>'+ item.unitName +' </span></div><div><span class="th">Class Start :  </span><span>'+ item.classStart +'</span></div><div><span class="th">Class End :  </span><span>'+ item.classEnd +'</span></div></div>'
 
   });
-
-  console.log(tableHtml)
 
   
   $("#timetalbeCard").html(tableHtml)
--- WebApp/js/login.js
+++ WebApp/js/login.js
@@ -5,34 +5,47 @@ $(document).ready(function () {
   $("#logoAIC").click(function(){
     $("#logoSyd").addClass("imgGrayScale"); 
   if ($("#logoAIC").hasClass("imgGrayScale"))
-    $("#logoAIC").removeClass("imgGrayScale");
+  {  
+  $("#logoAIC").removeClass("imgGrayScale");
+    college = 'AIC'
+  
+  }
   }
 );
   
   $("#logoSyd").click(function(){
     $("#logoAIC").addClass("imgGrayScale"); 
   if ($("#logoSyd").hasClass("imgGrayScale"))
+  {
     $("#logoSyd").removeClass("imgGrayScale");
+    college = 'SC'
+  }
   }
 );
+
   
   // set listener to login button
   $("#loginBtn").click(async function () {
+    
     let data = {
       studentID: $("#studentID").val(),
       email: $("#email").val(),
+      college: college
     };
+    console.log(data);
 
     //fetch student permission from api server
     const response = await fetch(
       "http://localhost:3000/api/v1/users/getUserDetail",
       {
+        // 192.168.1.47
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
       }
     );
     let student = await response.json();
+    console.log(response.json());
 
     if (!student.data) {
       alert("Please check you student ID or Email address again.");
@@ -44,6 +57,8 @@ $(document).ready(function () {
     localStorage.setItem("studentDetail", JSON.stringify(student.data));
 
     // open homepage in new window
+    // window.location.href("./home.html");
     window.open("./home.html", "_self");
+    
   });
 });
